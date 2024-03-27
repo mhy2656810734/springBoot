49137e30c2bbfc242e2ba5164d05365d1c66d820
mahaiyang <2656810734@qq.com>
2024-03-27 15:38:07 +0800

    新增了加盐加密的内容

---
+++ src/main/java/com/example/demo/moduls/PasswordTools.java
@@ -0,0 +1,77 @@
+package com.example.demo.moduls;
+
+import org.springframework.util.DigestUtils;
+import org.springframework.util.StringUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.UUID;
+
+/**
+ * 加盐加密的工具类
+ * @author mahaiyang
+ * @date 2024/3/27 15:20
+ */
+public class PasswordTools {
+    /**
+     * 加密方法
+     * @param password 用户传入的初始密码
+     * @return
+     */
+    public static String encrypt(String password) {
+        // 1.获取UUID,生成随机盐值
+        String salt = UUID.randomUUID().toString().replace("-","");
+        // 2.根据初始密码和随机盐值生成加盐加密的密码
+        String finaPassword = DigestUtils.md5DigestAsHex((salt+password).getBytes(StandardCharsets.UTF_8));
+        // 3.生成最终密码
+        String dbPassword = salt+"$"+finaPassword;
+        return dbPassword;
+    }
+
+    /**
+     * 加盐加密方法重载
+     * @param salt 随机盐值
+     * @param password 初始密码
+     * @return
+     */
+    public static String encrypt(String salt,String password) {
+        String finalPassword = DigestUtils.md5DigestAsHex((salt+password).getBytes(StandardCharsets.UTF_8));
+        return salt+"$"+finalPassword;
+    }
+
+    /**
+     * 加盐解密
+     * @param password 原始密码
+     * @param dbPassword 数据库中密码
+     * @return
+     */
+    public static  boolean decrypt(String password,String dbPassword) {
+        // 1.参数校验
+        if (StringUtils.hasLength(password) && StringUtils.hasLength(dbPassword) &&
+                dbPassword.length() == 65 && dbPassword.contains("$")) {
+            // 2.分割数据库中的密码
+            String[] arrPassword = dbPassword.split("\\$");
+            // 得到盐值
+            String salt = arrPassword[0];
+            if (dbPassword.equals(PasswordTools.encrypt(salt,password))) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("请输入您的密码");
+            String dbPassword = PasswordTools.encrypt("123456");
+            String password = scan.next();
+            if (PasswordTools.decrypt(password,dbPassword)) {
+                System.out.println("密码输入正确！！");
+                break;
+            }
+            System.out.println("密码输入错误，请重新尝试！");
+        }
+    }
+}
